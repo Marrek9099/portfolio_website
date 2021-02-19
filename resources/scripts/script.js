cd6bf22a7590fd99637bbd906af5878ab1b8ed3d
Dimitar <dimitarspasovsg@gmail.com>
2021-02-19 22:33:38 +0800

    Clean up CSS and HTML

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,7 +1,9 @@
-const p = document.querySelectorAll('#projects p')
+// Display project details when website visitors click on a specific project name
 
-p.forEach( p => {
-    p.addEventListener('click', function() {
+const projects = document.querySelectorAll('#projects h3')
+
+projects.forEach( project => {
+    project.addEventListener('click', function() {
        this.childNodes[0].classList.toggle('rotate');
        this.parentNode.childNodes[3].classList.toggle('visible');
     });
